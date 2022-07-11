3c452b899ee89fe1f296621a4daff39971f6e95c
Andrew Hall <hndrewaall@gmail.com>
2022-07-11 18:10:38 -0400

    Temple 7/12/22 (#16)

--- authbot/authbot.py
+++ authbot/authbot.py
@@ -18,7 +18,7 @@ from slack_sdk.errors import SlackApiError
 SYNC_MAP = {
     "all-holidays-ticket-2022-pass": "AllHolidays2022",
     "spring-2022-benefactors-support-the-satanic-estate": "Benefactors",
-    "temple-7-05-22-the-devils-railroad-satanic-aid-in-crisis": "Temple5July22",
+    "temple-7-12-22-therapy-with-satan": "Temple12July22",
 }
 
 
